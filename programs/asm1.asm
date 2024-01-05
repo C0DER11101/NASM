75e783953314d872723f27bc063cbe5d7bbd53b4
Priyanuj Bora <while.alive.program@gmail.com>
2024-01-05 06:08:46 +0530

    First program in assembly

--- programs/asm1.asm
+++ programs/asm1.asm
@@ -4,6 +4,6 @@ section .text   ; contains the actual code of our program
 global _start
 
 _start:
-	MOV eax, 1  ; move into register eax the value 1
-	MOV ebx, 1  ; move into ebx the value 1
-	INT 80h   ; h stands for hex
+	MOV eax, 1  ; move into register eax the value 1(this is sys_exit)
+	MOV ebx, 1  ; move into ebx the value 1(this is exit status code)
+	INT 80h   ; h stands for hex(so 80h is basically 0x80)

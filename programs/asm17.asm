fe73ce248cf9ebeb041c4495dd6c726a307ccf2b
Priyanuj Bora <while.alive.program@gmail.com>
2024-01-18 08:39:57 +0530

    Basics of x86 functions or procedures

---
+++ programs/asm17.asm
@@ -0,0 +1,20 @@
+; basics of functions/procedures in x86
+
+section .data
+
+section .text
+
+	global main
+
+	addTwo:
+		ADD eax, ebx
+		RET
+		
+	main:
+		MOV eax, 4
+		MOV ebx, 1
+		CALL addTwo
+		
+		MOV ebx, eax
+		MOV eax, 1
+		INT 80h
